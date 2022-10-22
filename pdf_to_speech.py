31ae12b9394c7d0ad2b66fa01bc371b6a8355645
Yash Chaudhary <ygcjal@gmail.com>
2022-10-23 01:05:08 +0530

    UI changes in main.py

---
+++ pdf_to_speech.py
@@ -0,0 +1,27 @@
+# importing the modules
+import PyPDF2
+import pyttsx3
+
+
+class PDFReader:
+    def __init__(self, file_path):
+        # path of the PDF file
+        self.path = open(file_path)
+
+    def translate_pdf(self):
+        # creating a PdfFileReader object
+        pdf_reader = PyPDF2.PdfFileReader(self.path)
+
+        # the page with which you want to start
+        # this will read the book of 25th page.
+        from_page = pdf_reader.getPage(30)
+
+        # extracting the text from the PDF
+        entry_text = from_page.extractText()
+
+        return str(entry_text)
+
+    # reading the text
+    # speak = pyttsx3.init()
+    # speak.say(entry_text)
+    # speak.runAndWait()
